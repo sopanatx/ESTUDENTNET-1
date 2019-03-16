c6c40e6a6bce65697dd90cfce73c7d80927dcc65
pleumkungz <pleumkungz@Pleumkungz.local>
2019-03-16 21:12:47 +0700

    Added Test Resonsive Background

--- resources/views/nindex.blade.php
+++ resources/views/nindex.blade.php
@@ -1,20 +1,38 @@
-<link href="//netdna.bootstrapcdn.com/bootstrap/3.1.0/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
-<script src="//netdna.bootstrapcdn.com/bootstrap/3.1.0/js/bootstrap.min.js"></script>
-<script src="//code.jquery.com/jquery-1.11.1.min.js"></script>
-<!------ Include the above in your HEAD tag ---------->
+<html xmlns="http://www.w3.org/1999/xhtml"><head><meta charset="utf-8"><meta http-equiv="X-UA-Compatible" content="IE=edge"><title>
+       ระบบตรวจสอบคะแนนพฤติกรรมนักเรียน
+    </title>
+    <!-- Tell the browser to be responsive to screen width -->
+    <meta content="width=device-width, initial-scale=1" name="viewport"><link href="http://fonts.googleapis.com/css?family=Lato:100italic,100,300italic,300,400italic,400,700italic,700,900italic,900" rel="stylesheet"><link rel="stylesheet" href="../Themes/Login/assets/bootstrap/css/bootstrap.min.css">
 
-<!-- Submitted to Feature  March 2, 2014  11:20pm -->
-<div class="container">
-    <div class="row">
-        <h2>Responsive Background Images</h2>
-        <p>
-            <a href="http://bootsnipp.com/iframe/x95Q" class="btn btn-lg btn-success" target="_blank"><span class="glyphicon glyphicon-fullscreen"></span> Full screen</a>
-        </p>
-        <p>
-            <br><br>
-            <a href="http://validator.w3.org/check?uri=http://bootsnipp.com/iframe/x95Q&ss=1">
-                <span class="glyphicon glyphicon-check green"></span> HTML<sup>5</sup></a>
-        </p>
+    <!-- Not required: presentational-only.css only contains CSS for prettifying the demo -->
+    <link rel="stylesheet" href=.."/Themes/presentational-only.css">
 
+    <!-- responsive-full-background-image.css stylesheet contains the code you want -->
+    <link rel="stylesheet" href="/Themes/background-resonsive.css"></head>
+<link href="assets/plugins/pace/pace-theme-flash.css" rel="stylesheet" type="text/css" />
+<link href="assets/plugins/bootstrap/css/bootstrap.min.css" rel="stylesheet" type="text/css" />
+<link href="assets/plugins/font-awesome/css/font-awesome.css" rel="stylesheet" type="text/css" />
+<link href="assets/fonts/stylesheet.css" rel="stylesheet" type="text/css" />
+<link href="assets/plugins/swiper/css/swiper.css" rel="stylesheet" type="text/css" media="screen" />
+<link href="assets/plugins/text-rotate/simpletextrotator.css" rel="stylesheet" type="text/css" media="screen" />
+<script src="https://cdnjs.cloudflare.com/ajax/libs/sweetalert/2.1.2/sweetalert.min.js"></script>
+
+<link class="main-stylesheet" href="pages/css/pages.css" rel="stylesheet" type="text/css" />
+<link class="main-stylesheet" href="pages/css/pages-icons.css" rel="stylesheet" type="text/css" />
+<body>
+
+<!-- /.login-logo -->
+<section class="container center-block center-margin">
+    <div class="form-horizontal form-control">
+        <h1 class="center-margin center-block"> text </h1>
     </div>
-</div>
+    </section>
+</section>
+
+<script src="http://ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js"></script>
+<script src="../Themes/Login/assets/bootstrap/js/bootstrap.min.js"></script>
+
+
+
+</body>
+</html>
