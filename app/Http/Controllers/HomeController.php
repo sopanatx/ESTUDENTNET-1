81bb59b3e1c23f133aad4601df3e59e6abdd0213
WasuthornX <sopanus2014@gmail.com>
2020-10-01 08:38:53 +0700

    Refactor : Remove code comment

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -35,15 +35,8 @@ class HomeController extends Controller
     public function search(Request $request)
     {
         $stuid = $request->stu_id;
-    //    $chklogshow = Checkhistory::search('std_id',$stuid)->get();
         $chkshow = checkstd::findOrFail($stuid);
-
         return view('result')->with(array('chklogshow'=>$chklogshow,'chkshow '=>$chkshow ));
-        //$std_id = Input::get ( 'std_id' );
-        //$stu = DB::table('student')->where('std_id', 'LIKE','%'.$std_id.'%')->first();
-        //if(count($stu) > 0)
-          //  return view('result')->withDetails($stu)->withQuery ( $std_id );
-       // else return view ('result')->withMessage('No Details found. Try to search again !');
     }
 
 
